44e39a980f9e6cb1ea1e72ed7dc8b8ad6bd7cb2c
chuksDev <edokichuks@gmail.com>
2025-08-07 14:05:02 +0100

    desktop view completed

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
-import 'package:xpert_funding_test/firebase_options.dart';
 import 'package:xpert_funding_test/src/core/config/exceptions/overall_app_error.dart';
-import 'package:xpert_funding_test/src/core/device_features/device_feature_exports.dart';
 import 'package:xpert_funding_test/src/core/helpers/helper_functions.dart';
 import 'package:xpert_funding_test/src/core/router/router.dart';
 import 'package:xpert_funding_test/src/core/services/local_storage.dart/local_storage_export.dart';
@@ -27,20 +23,12 @@ import 'package:xpert_funding_test/src/general_widgets/general_widget_exports.da
 // Add a navigator key to access navigation state globally
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
-@pragma("vm:entry-point")
-Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  await Firebase.initializeApp();
-  DeviceNotification().firebaseInit();
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-}
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
   await Hive.initFlutter();
   await Hive.openBox(LocalStoreKeysManger.appBox.rawValue);
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   AppErrorLog.init();
   runApp(
     ProviderScope(
@@ -64,24 +52,6 @@ class MyApp extends ConsumerStatefulWidget {
 
 class _MyAppState extends ConsumerState<MyApp> with WidgetsBindingObserver {
   final _controller = OverLayLoaderController();
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-      DeviceNotification().firebaseInit(ref: ref);
-      //!Question: Is the forgroundMessage method only for iOS?
-      DeviceNotification().forgroundMessage();
-      // final user = await UserStoreManager.getSavedUser();
-      // if (user != null) {
-      //   await UserStoreManager.saveUser(
-      //       user: user.copyWith(isLoggedIn: false));
-      // }
-      if (mounted) HelperFunctions().activateTimer(dispose: false);
-      // _activateTimer(dispose: false);
-    });
-    WidgetsBinding.instance.addObserver(this);
-  }
 
   @override
   void dispose() {
--- lib/src/features/account/widgets/account_card_desktop.dart
+++ lib/src/features/account/widgets/account_card_desktop.dart
@@ -18,8 +18,8 @@ class AccountCardDesktop extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 500.w, // Desktop wider card
-      padding: EdgeInsets.all(16.r),
+      width: 632.w,
+      padding: EdgeInsets.symmetric(vertical: 16.h),
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage(AppImages.topshadow),
@@ -41,159 +41,225 @@ class AccountCardDesktop extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Top Badges
-          Row(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 3.h),
-                decoration: BoxDecoration(
-                  color: account.currentPhase == 0
-                      ? Color(0xff053321)
-                      : Color(0xff11285F),
-                  border: Border.all(
-                    color: account.currentPhase == 0
-                        ? Color(0xff75DFA7)
-                        : Color(0xff80A4FE),
-                  ),
-                  borderRadius: BorderRadius.circular(12.r),
-                ),
-                child: Text(
-                  account.currentPhase == 0 ? "Funded" : "On Challenge",
-                  style: TextStyle(
-                    color: account.currentPhase == 0
-                        ? Color(0xff75DFA7)
-                        : Color(0xff80A4FE),
-                    fontSize: 12.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ),
-            ],
-          ),
-
-          SizedBox(height: 16.h),
-
-          // Title + PRO
-          Row(
-            children: [
-              Text(
-                account.accountName ?? '',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14.sp,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              Spacing.width(11),
-              if (account.isProAccount == true) ...[
-                SizedBox(width: 6.w),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 6.w, vertical: 2.h),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8.r),
-                    gradient: LinearGradient(
-                      colors: [
-                        Color(0xff0249FE).withOpacity(0.3),
-                        Color(0xff80A4FE).withOpacity(0.3),
-                      ],
-                    ),
-                    border: GradientBoxBorder(
-                      width: 1.14,
-                      gradient: LinearGradient(
-                        colors: [Color(0xff0249FE), Color(0xff80A4FE)],
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // Title + PRO
+                Row(
+                  children: [
+                    Text(
+                      account.accountName ?? '',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
                       ),
                     ),
+                    Spacing.width(11),
+                    if (account.isProAccount == true) ...[
+                      SizedBox(width: 6.w),
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 6.w,
+                          vertical: 2.h,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(800.r),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xff0249FE).withOpacity(0.3),
+                              Color(0xff80A4FE).withOpacity(0.3),
+                            ],
+                          ),
+                          border: GradientBoxBorder(
+                            width: 1.14,
+                            gradient: LinearGradient(
+                              colors: [Color(0xff0249FE), Color(0xff80A4FE)],
+                            ),
+                          ),
+                        ),
+                        child: Transform.scale(
+                          scale: 1.8,
+                          child: AppImageView(
+                            svgPath: AppImages.pro,
+                            width: 24,
+                            height: 17,
+
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 10.w,
+                    vertical: 3.h,
                   ),
-                  child: Transform.scale(
-                    scale: 1.8,
-                    child: AppImageView(
-                      svgPath: AppImages.pro,
-                      width: 24,
-                      height: 17,
-                      fit: BoxFit.cover,
+                  decoration: BoxDecoration(
+                    color: account.currentPhase == 0
+                        ? Color(0xff053321)
+                        : Color(0xff11285F),
+                    border: Border.all(
+                      color: account.currentPhase == 0
+                          ? Color(0xff75DFA7)
+                          : Color(0xff80A4FE),
+                    ),
+                    borderRadius: BorderRadius.circular(1200.r),
+                  ),
+                  child: Text(
+                    account.currentPhase == 0 ? "Funded" : "On Challenge",
+                    style: TextStyle(
+                      color: account.currentPhase == 0
+                          ? Color(0xff75DFA7)
+                          : Color(0xff80A4FE),
+                      fontSize: 12.sp,
+                      fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
               ],
-            ],
+            ),
           ),
 
           SizedBox(height: 8.h),
 
-          Text(
-            "\$${account.size?.toStringAsFixed(0) ?? '0'}",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 24.sp,
-              fontWeight: FontWeight.w800,
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Text(
+              "\$${account.size?.toStringAsFixed(0) ?? '0'}",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w800,
+              ),
             ),
           ),
 
-          SizedBox(height: 16.h),
+          SizedBox(height: 8.h),
 
           // Balance, Bought Date, ID
-          Text.rich(
-            TextSpan(
-              text: 'Balance : ',
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Row(
+              spacing: 12,
               children: [
-                TextSpan(
-                  text: '\$${account.balance?.toStringAsFixed(0)}',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
+                Text.rich(
+                  TextSpan(
+                    text: 'Balance : ',
+                    children: [
+                      TextSpan(
+                        text: '\$${account.balance?.toStringAsFixed(0)}',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ],
                   ),
+                  style: TextStyle(
+                    color: Colors.white.withOpacity(0.6),
+                    fontSize: 17.sp,
+                  ),
+                ),
+                Icon(
+                  Icons.circle,
+                  size: 10.r,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+
+                Row(
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        text: 'Bought : ',
+                        children: [
+                          TextSpan(
+                            text:
+                                ' ${account.createdAt != null ? "${account.createdAt!.month}/${account.createdAt!.day}/${account.createdAt!.year}" : ''}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 17.sp,
+                            ),
+                          ),
+                        ],
+                      ),
+                      style: TextStyle(
+                        fontSize: 17.sp,
+                        color: Colors.white.withOpacity(0.6),
+                      ),
+                    ),
+
+                    Spacing.widthS(),
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.white.withOpacity(0.3),
+                      size: 20,
+                    ),
+                  ],
+                ),
+                Icon(
+                  Icons.circle,
+                  size: 10.r,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+
+                Row(
+                  children: [
+                    Text.rich(
+                      TextSpan(
+                        text: 'ID : ',
+                        children: [
+                          TextSpan(
+                            text: ' ${account.id ?? ''}',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700,
+                              fontSize: 17.sp,
+                            ),
+                          ),
+                        ],
+                      ),
+                      style: TextStyle(
+                        fontSize: 17.sp,
+                        color: Colors.white.withOpacity(0.6),
+                      ),
+                    ),
+                    Spacing.widthS(),
+                    Icon(
+                      Icons.info_outline,
+                      color: Colors.white.withOpacity(0.3),
+                      size: 20,
+                    ),
+                  ],
                 ),
               ],
             ),
-            style: TextStyle(color: Colors.white.withOpacity(0.6)),
           ),
 
-          SizedBox(height: 6.h),
+          SizedBox(height: 18.h),
 
-          Text.rich(
-            TextSpan(
-              text: 'Bought : ',
+          Container(
+            // height: 60.h,
+            padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 11.h),
+            decoration: BoxDecoration(
+              color: Colors.white.withValues(alpha: 0.05),
+            ),
+            child: Row(
+              spacing: 12.w,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                TextSpan(
-                  text:
-                      ' ${account.createdAt != null ? "${account.createdAt!.month}/${account.createdAt!.day}/${account.createdAt!.year}" : ''}',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
+                _phaseBadge("Evaluation 1"),
+                _phaseBadge("Evaluation 2"),
+                _phaseBadge("Master Account", isMaster: true),
               ],
             ),
-            style: TextStyle(color: Colors.white.withOpacity(0.6)),
-          ),
-
-          SizedBox(height: 6.h),
-
-          Text.rich(
-            TextSpan(
-              text: 'ID : ',
-              children: [
-                TextSpan(
-                  text: ' ${account.id ?? ''}',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-              ],
-            ),
-            style: TextStyle(color: Colors.white.withOpacity(0.6)),
-          ),
-
-          SizedBox(height: 16.h),
-
-          // Phase Badges - Desktop only
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              _phaseBadge("Evaluation 1"),
-              _phaseBadge("Evaluation 2"),
-              _phaseBadge("Master Account", isMaster: true),
-            ],
           ),
 
           SizedBox(height: 16.h),
@@ -201,14 +267,14 @@ class AccountCardDesktop extends StatelessWidget {
           // Dashboard button
           Container(
             width: double.infinity,
-            height: 32.h,
-            padding: EdgeInsets.symmetric(vertical: 8.h, horizontal: 16.w),
+            height: 48.h,
+            margin: EdgeInsets.symmetric(horizontal: 16.w),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(8.r),
               color: Colors.white,
-              gradient: LinearGradient(
-                colors: [Colors.white, Color(0xffCCDBFF)],
-              ),
+              // gradient: LinearGradient(
+              //   colors: [Colors.white, Color(0xffCCDBFF)],
+              // ),
               border: GradientBoxBorder(
                 gradient: LinearGradient(
                   colors: [Colors.white.withOpacity(0.2), Colors.white],
@@ -220,8 +286,8 @@ class AccountCardDesktop extends StatelessWidget {
               children: [
                 AppImageView(
                   margin: EdgeInsets.only(top: 2),
-                  height: 14,
-                  width: 14,
+                  height: 16,
+                  width: 16,
                   svgPath: AppImages.dashboard,
                 ),
                 Spacing.widthS(),
@@ -244,20 +310,38 @@ class AccountCardDesktop extends StatelessWidget {
     return Container(
       padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 4.h),
       decoration: BoxDecoration(
-        color: isMaster
-            ? Color(0xff5865F2).withOpacity(0.2)
-            : Color(0xff11285F),
+        color: isMaster ? Color(0xff4652BB) : null,
+        gradient: isMaster
+            ? null
+            : LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color(0xff5865F2),
+                  Color(0xff5865F2).withValues(alpha: 0.2),
+                ],
+              ),
+        border: GradientBoxBorder(
+          width: 1,
+          gradient: LinearGradient(
+            colors: [
+              Color(0xff5865F2),
+              Color(0xff5865F2).withValues(alpha: 0.2),
+              Color(0xff5865F2).withValues(alpha: 0.2),
+            ],
+          ),
+        ),
         borderRadius: BorderRadius.circular(12.r),
       ),
       child: Row(
         children: [
           if (isMaster)
-            AppImageView(svgPath: AppImages.lock, width: 13.r, height: 13.r)
+            AppImageView(svgPath: AppImages.lock, width: 16.r, height: 16.r)
           else
             AppImageView(
               svgPath: AppImages.evaluate,
-              width: 13.r,
-              height: 13.r,
+              width: 16.r,
+              height: 16.r,
             ),
           Spacing.width(6.w),
           Text(
